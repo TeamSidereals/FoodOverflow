6e85f63246cf043f51d354c5f1eb42626de80d37
DUCVN <nguyenvietduc24@yahoo.com.vn>
2022-08-06 13:01:55 +0700

    set up files

---
+++ src/main/java/com/teamsidereals/foodoverflow/item/FoodOverflowFoodItem.java
@@ -0,0 +1,9 @@
+package com.teamsidereals.foodoverflow.item;
+
+import net.minecraft.item.Item;
+
+public class FoodOverflowFoodItem extends Item {
+    public FoodOverflowFoodItem(Properties p_i48487_1_) {
+        super(p_i48487_1_);
+    }
+}
