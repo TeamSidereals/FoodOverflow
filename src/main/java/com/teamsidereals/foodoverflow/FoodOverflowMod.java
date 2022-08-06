efb82b1b1ca7ccea0b8948650775e210fa53f259
DUCVN <nguyenvietduc24@yahoo.com.vn>
2022-08-06 12:48:19 +0700

    set up files

--- src/main/java/com/teamsidereals/foodoverflow/FoodOverflowMod.java
+++ src/main/java/com/teamsidereals/foodoverflow/FoodOverflowMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.teamsidereals.foodoverflow;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,11 +19,10 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod(FoodOverflowMod.MODID)
 public class FoodOverflowMod
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "foodoverflow";
 
     public FoodOverflowMod() {
         // Register the setup method for modloading
@@ -41,34 +40,21 @@ public class FoodOverflowMod
 
     private void setup(final FMLCommonSetupEvent event)
     {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
     {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
     }
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
     }
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
@@ -77,8 +63,6 @@ public class FoodOverflowMod
     public static class RegistryEvents {
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
         }
     }
 }
