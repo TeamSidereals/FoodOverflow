09c52249005c36b4c0400fa8efe959c8a5334f95
DUCVN <nguyenvietduc24@yahoo.com.vn>
2022-08-14 16:59:31 +0700

    Add Caramel cooking recipe

---
+++ src/main/java/io/github/teamsidereals/foodoverflow/titleentity/AgingChamberTile.java
@@ -0,0 +1,10 @@
+package io.github.teamsidereals.foodoverflow.titleentity;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+
+public class AgingChamberTile extends TileEntity {
+    public AgingChamberTile(TileEntityType<?> p_i48289_1_) {
+        super(p_i48289_1_);
+    }
+}
